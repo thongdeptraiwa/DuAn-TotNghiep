c3d39f6680fd0541059128a1091037f0b1269a67
taivt2004 <vtt2004abc@gmail.com>
2024-08-21 16:49:09 +0700

    cap nhat tiep

--- src/components/Screens/Home.jsx
+++ src/components/Screens/Home.jsx
@@ -1,7 +1,7 @@
 import { FlatList, Image, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import HomeS from '../styles/HomeS';
-import Post from '../../flatlist/Post';
+import Post from '../../items/Post';
 import Icon from 'react-native-vector-icons/Ionicons'; // Hoặc một bộ icon khác
 let date = new Date().toDateString()
 const listPost = [
